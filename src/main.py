910401c4d840e41ee6cef1a4121cbfd59b6a9427
Diego Ignacio Rojas Gonzalez <121054326+DiegoRojasGonzalez@users.noreply.github.com>
2023-06-26 19:56:31 -0400

    Implement start logic.
    Implement stopwatch start logic.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
 from kivy.app import App
-from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from pomodoro import Pomodoro
+
+class MainApp(App):
+    def build(self):
+        return BoxLayout()
+
+    def start_pomodoro(self):
+        pomodoro = Pomodoro("Clasico", 25, 5, 15, 4, 0, "Focus", 0)
+        pomodoro.start()
+
+if __name__ == '__main__':
+    MainApp().run()
+
+
