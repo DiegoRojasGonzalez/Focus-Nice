b530d210a554077cc1aea71a267e35b909b1dce9
Diego Ignacio Rojas Gonzalez <121054326+DiegoRojasGonzalez@users.noreply.github.com>
2023-06-27 19:39:20 -0400

    refactorización
    refactoring how works the views (Now the layout will only be controlled by archive.kv)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
-from kivy.lang import Builder
-from kivymd.app import MDApp
+from kivymd.app import MDApp                                #incluimos la libreria de kivymd para Material Design
+from kivy.lang import Builder                               #incluimos el builder para iniciar el archivo kv con los estilos
+from kivy.uix.screenmanager import ScreenManager, Screen    #incluimos el screen manager para cambiar de pantalla
+from kivy.core.window import Window                         #incluimos la libreria de window para cambiar el tamaño de la ventana 
+from pomodoro import Pomodoro                               #incluimos la clase pomodoro para modificar el objeto
 
-class FocusNiceApp(MDApp):
-    def build(self):
-        return Builder.load_file("style.kv")
+Window.size = (360, 640)                                    #cambiamos el tamaño de la ventana
 
-    def on_start(self):
-        self.fps_monitor_start()
+class UIStorage(ScreenManager):                             #creamos la clase UIStorage que hereda de ScreenManager
+    pass
 
+class MainApp(MDApp):                                       #creamos la clase MainApp que hereda de MDApp
+    
+    def build(self):                                        #creamos el metodo build que se encarga de iniciar la aplicacion
+        Builder.load_file("style.kv")                       #cargamos el archivo kv con los estilos
+        return UIStorage()                                  #retornamos la clase Ui
 
-FocusNiceApp().run()
+        
+    #Aqui van las funciones 
+    # 
+    #
+
+if __name__ == "__main__":                                  #verificamos que el archivo se este ejecutando directamente
+    MainApp().run()                                         #ejecutamos la aplicacion
--- src/pomodoro.py
+++ src/pomodoro.py
@@ -1,5 +1,4 @@
 import time
-
 class Pomodoro:
     def __init__(self, Name, Time_focus, Time_break, Time_longbreak, Cycles, Cycle_Actually, State, Time):
         self.Name = Name
@@ -10,37 +9,5 @@ class Pomodoro:
         self.Cycle_Actually = 0
         self.State = "Focus"
         self.Time = 0
-
-    def start(self):
-        if self.State == "Focus":
-            self.Time = self.Time_focus
-        elif self.State == "Break":
-            self.Time = self.Time_break
-        elif self.State == "Long Break":
-            self.Time = self.Time_longbreak
-
-        while self.Time > 0:
-            print(f"Time remaining: {self.Time} minutes")
-            time.sleep(60)  # Sleep for 1 minute
-            self.Time -= 1
-
-        self.update_state()
-
-    def update_state(self):
-        if self.State == "Focus":
-            self.Cycle_Actually += 1
-            if self.Cycle_Actually == self.Cycles:
-                self.State = "Long Break"
-                self.Cycle_Actually = 0
-            else:
-                self.State = "Break"
-        elif self.State == "Break":
-            self.State = "Focus"
-        elif self.State == "Long Break":
-            self.State = "Focus"
-
-        self.start()
-
-
 
 
