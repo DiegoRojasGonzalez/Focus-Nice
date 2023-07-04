ed879b6cab6e75229724304b24b784407b689795
Diego Ignacio Rojas Gonzalez <121054326+DiegoRojasGonzalez@users.noreply.github.com>
2023-07-03 20:15:04 -0400

    Add start pomodoro function
    - Add function "start_pomodoro"
    - Change pomodoro class
    - Add "Start" Method

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder                               #incluimos el builde
 from kivy.uix.screenmanager import ScreenManager, Screen    #incluimos el screen manager para cambiar de pantalla
 from kivy.core.window import Window                         #incluimos la libreria de window para cambiar el tamaño de la ventana 
 from pomodoro import Pomodoro                               #incluimos la clase pomodoro para modificar el objeto
+from kivy.clock import Clock
 
 Window.size = (360, 640)                                    #cambiamos el tamaño de la ventana
 
@@ -15,10 +16,12 @@ class MainApp(MDApp):                                       #creamos la clase Ma
         Builder.load_file("style.kv")                       #cargamos el archivo kv con los estilos
         return UIStorage()                                  #retornamos la clase Ui
 
-        
-    #Aqui van las funciones 
-    # 
-    #
+    def start_pomodoro(self):
+        pomodoro = Pomodoro("Clasico", 25, 5, 15, 4)
+        Clock.schedule_once(pomodoro.Start())
+        #pomodoro.Start()
+        print(f"El pomodoro se llama {pomodoro.Name}")
+        print("La funcion start del pomodoro_start (main.py) se ejecuto")
 
 if __name__ == "__main__":                                  #verificamos que el archivo se este ejecutando directamente
     MainApp().run()                                         #ejecutamos la aplicacion
