e6dca1f4e4cbb45de94d12abace1b5739fa9b6c7
Diego Ignacio Rojas Gonzalez <121054326+DiegoRojasGonzalez@users.noreply.github.com>
2023-07-09 21:28:59 -0400

    rebuild
    -Fix small bugs
    -Add Start Pomodoro Function

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from kivymd.app import MDApp                                #incluimos la librer
 from kivy.lang import Builder                               #incluimos el builder para iniciar el archivo kv con los estilos
 from kivy.uix.screenmanager import ScreenManager, Screen    #incluimos el screen manager para cambiar de pantalla
 from kivy.core.window import Window                         #incluimos la libreria de window para cambiar el tamaño de la ventana 
-from pomodoro import Pomodoro                               #incluimos la clase pomodoro para modificar el objeto
 from kivy.clock import Clock
 
 Window.size = (360, 640)                                    #cambiamos el tamaño de la ventana
@@ -11,17 +10,45 @@ class UIStorage(ScreenManager):                             #creamos la clase UI
     pass
 
 class MainApp(MDApp):                                       #creamos la clase MainApp que hereda de MDApp
+    # Zona de definicion de variables
+    cicle_pom = 1500 #tiempo de trabajo
+    brake_pom = 300 #tiempo de descanso corto
+    long_break = 900 #tiempo de descanso largo
+    #
     
+    state = False #estado del pomodoro en falso
+
     def build(self):                                        #creamos el metodo build que se encarga de iniciar la aplicacion
         Builder.load_file("style.kv")                       #cargamos el archivo kv con los estilos
+        Clock.schedule_interval(self.update_text_time, 0)   #llamamos al metodo update_text_time cada segundo
         return UIStorage()                                  #retornamos la clase Ui
 
-    def start_pomodoro(self):
-        pomodoro = Pomodoro("Clasico", 25, 5, 15, 4)
-        Clock.schedule_once(pomodoro.Start())
-        #pomodoro.Start()
-        print(f"El pomodoro se llama {pomodoro.Name}")
-        print("La funcion start del pomodoro_start (main.py) se ejecuto")
+    def on_start(self):
+        self.fps_monitor_start()
+
+    # Metodo start
+    def update_text_time(self, event):                       #creamos el metodo update_text_time que recibe el tiempo
+        if self.state == True:                              #si el estado es falso                                           
+            print("✔")
+            #POMODORO ACTIVO
+            #
+            #
+            #
+            self.cicle_pom -= event                              
+            time = self.cicle_pom                                
+            minutes, seconds = divmod(time, 60)
+            seconds = int(seconds)
+            minutes = int(minutes)
+            if len(str(seconds)) == 1:
+                seconds = '0' + str(seconds)
+            if len(str(minutes)) == 1:
+                minutes = '0' + str(minutes)
+            time = f'{minutes}:{seconds}'
+            self.root.ids.time.text = str(time)                  #cambiamos el texto de la etiqueta time por el tiempo        
+        pass
+        
+    def start_pomodoro(self):                               #creamos el metodo start_pomodoro
+        self.state = True                                   #cambiamos el estado a verdadero
 
 if __name__ == "__main__":                                  #verificamos que el archivo se este ejecutando directamente
     MainApp().run()                                         #ejecutamos la aplicacion
